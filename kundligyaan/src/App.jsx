72e87f46853a5851ce41ebbd97475016924dec67
DKSHRI <DEVANSHU.SHRIVASTAVA@s.amity.edu>
2024-12-17 19:00:52 +0530

    all file

--- kundligyaan/src/App.jsx
+++ kundligyaan/src/App.jsx
@@ -1,27 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import Cursor from "./components/CustomCursor";
-import AstrologerProfile from "./components/AstrologerProfile";
-import PastConsultations from "./components/PastConsultations";
-import AppointmentHistory from "./components/AppointmentHistory";
-import SupportPage from "./components/SupportPage";
-import './App.css';
+         
+import Navbar from "./Navbar.jsx"
+import MainSection from "./MainSection.jsx"
+import AstrologyCarousel from "./AstrologyCarousel.jsx"
+import Footer from "./Footer.jsx"
 function App() {
-  return (
-    <Router>
-      <Cursor />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/AstrologerProfile" element={<AstrologerProfile />} />
-        <Route path="/PastConsultations" element={<PastConsultations />}/>
-        <Route path="/SupportPage" element={<SupportPage/>}></Route>
-        <Route path="/AppointmentHistory" element={<AppointmentHistory/>}></Route>
-      </Routes>
-    </Router>
-  );
+return(
+ <>
+ <Navbar/>
+ <MainSection/>
+<AstrologyCarousel/>
+ <Footer/>
+ </>
+);
+
 }
 
 export default App;
--- kundligyaan/src/main.jsx
+++ kundligyaan/src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./Navbar.css";
+import './MainSection.css';
+import "./AstrologyCarousel.css";
+
+import "./Footer.css";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
+  </StrictMode>,
 )
